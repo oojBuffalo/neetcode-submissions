8af715a607358d8f599da36fed7cc97711ecb3d9
Elijah Wilt <elijahwilt.github@gmail.com>
2026-04-29 15:31:36 -0400

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n: int = len(nums) # Length of array
+        i: int = 0         # Read index for loop
+        j: int = 0         # Write index for loop
+
+        for i in range(n):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+
+        # The number of remaining elements is equal to the next write index
+        #     i.e. num elements written + 1 = next write index
+        return j
