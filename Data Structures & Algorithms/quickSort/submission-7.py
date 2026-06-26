d0eab30901286563853489b1d75d80b1fa307bfd
Elijah Wilt <elijahwilt.github@gmail.com>
2026-06-25 23:48:53 -0400

    Add: quickSort - submission-7

---
+++ Data Structures & Algorithms/quickSort/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def quickSort(self, pairs: List[Pair]) -> List[Pair]:
+        def helper(start: int, end: int):  
+            if start >= end:
+                return
+            
+            pivot = end
+            swap = start
+
+            for i in range(start, end):
+                if pairs[i].key < pairs[pivot].key:
+                    pairs[i], pairs[swap] = pairs[swap], pairs[i]
+                    swap += 1
+            
+            pairs[pivot], pairs[swap] = pairs[swap], pairs[pivot]
+            helper(start, swap - 1)
+            helper(swap + 1, end)
+
+        helper(0, len(pairs) - 1)
+        return pairs
+            
